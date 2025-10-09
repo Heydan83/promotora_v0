1be685a10c69d8c0031298a34b15d09d1e75a907
Adan <adan.ulloa@gmail.com>
2025-10-09 13:11:53 -0600

    commit 3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import logo from '$assets/logo.png';
 
 	let { children } = $props();
 </script>
@@ -8,5 +9,25 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<div>
+	<img src={logo} alt="logo casa 30-30" class="logo">
+</div>
+
+<style>
+	.logo {
+		max-width: 240px;
+		height: 100%;
+		width: 100%;
+		padding-left: 1rem;
+	}
+
+	@media (max-width: 991px) {
+		.logo {
+			max-height: 50px;
+			max-width: 100px;
+		}
+	}
+</style>
 
 {@render children?.()}
