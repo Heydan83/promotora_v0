c4144eff6d919ae63a516c08a36cb11f1e675144
Adan <adan.ulloa@gmail.com>
2025-10-09 15:56:09 -0600

    commit #4

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import logo from '$assets/logo.png';
+	import { hamburger } from '$assets';
 
 	let { children } = $props();
 </script>
@@ -10,24 +11,52 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div>
-	<img src={logo} alt="logo casa 30-30" class="logo">
+<div class="header">
+	<img src={logo} alt="logo casa 30-30" class="logo" />
+	<button>
+		<img src={hamburger} alt="" class="hamburger" />
+	</button>
 </div>
 
+{@render children?.()}
+
 <style>
+	.header {
+		display: flex;
+		justify-content: space-between;
+		height: 5rem;
+		align-items: center;
+	}
+
 	.logo {
-		max-width: 240px;
-		height: 100%;
-		width: 100%;
-		padding-left: 1rem;
+		max-width: 14rem;
+		max-height: 10rem;
+		margin: 1rem;
+		margin-left: 4rem;
+	}
+
+	.hamburger {
+		max-width: 2rem;
+		max-height: 2rem;
+		margin: 1rem;
 	}
 
 	@media (max-width: 991px) {
+		.header {
+			height: 2.5rem;
+		}
+
 		.logo {
-			max-height: 50px;
-			max-width: 100px;
+			max-height: 3.125rem;
+			max-width: 6.25rem;
+			margin: 0.5rem;
+			margin-left: 2rem;
+		}
+
+		.hamburger {
+			max-width: 1rem;
+			max-height: 1rem;
+			margin: 2rem;
 		}
 	}
 </style>
-
-{@render children?.()}
