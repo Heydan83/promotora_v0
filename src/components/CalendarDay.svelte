e4c32fc176807de7e1481766ac31f9dda11697f1
Adan <adan.ulloa@gmail.com>
2025-10-13 16:09:49 -0600

    commit #19

--- src/components/CalendarDay.svelte
+++ src/components/CalendarDay.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
 	interface Day {
-        year: number;
-        month: number;
+		year: number;
+		month: number;
 		day: number;
 	}
 
 	let { year, month, day }: Day = $props();
-    const currentDate = new Date();
-    currentDate.setHours(0, 0, 0, 0); 
-    const receivedDate = new Date(year, month - 1, day)
-    const today = currentDate.toDateString() === receivedDate.toDateString() ? 'today' : 'day';
-</script> 
+	const currentDate = new Date();
+	currentDate.setHours(0, 0, 0, 0);
+	const receivedDate = new Date(year, month - 1, day);
+	const today = currentDate.toDateString() === receivedDate.toDateString() ? 'today' : 'day';
+</script>
 
 <div class={today}>{day}</div>
 
@@ -19,19 +19,23 @@
 		background-color: var(--bg-grey-light-color);
 		border-radius: 10px;
 		text-align: center;
-        height: 5rem; 
+		height: 5rem;
 	}
 
-    .today {
-        background-color: black;
-        color: white;
+	.today {
+		background-color: black;
+		color: white;
 		border-radius: 10px;
 		text-align: center;
-        height: 5rem; 
-    }
+		height: 5rem;
+	}
 
 	@media (max-width: 991px) {
 		.day {
+			height: calc(5vh + 15px);
+		}
+
+		.today {
 			height: calc(5vh + 15px);
 		}
 	}
